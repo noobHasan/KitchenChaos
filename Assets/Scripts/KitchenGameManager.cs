d359fd328377471e069c694e32935e59e0cf6c1b
noobHasan <95558492+noobHasan@users.noreply.github.com>
2023-02-06 18:27:05 +0600

    9:04

--- Assets/Scripts/KitchenGameManager.cs
+++ Assets/Scripts/KitchenGameManager.cs
@@ -9,6 +9,8 @@ public class KitchenGameManager : MonoBehaviour
     public static KitchenGameManager Instance { get; private set; }
 
     public event EventHandler OnStateChanged;
+    public event EventHandler OnGamePaused;
+    public event EventHandler OnGameUnPaused;
 
     private enum State {
         WaitingToStart,
@@ -23,6 +25,18 @@ public class KitchenGameManager : MonoBehaviour
     private float countDownToStartTimer = 3f;
     private float gamePlayingTimer;
     private float gamePlayingTimerMax = 10f;
+    private bool isGamePaused = false;
+
+    private void Start()
+    {
+        GameInput.Instance.OnPauseAction += GameInput_OnPauseAction;   
+    }
+
+    private void GameInput_OnPauseAction(object sender, EventArgs e)
+    {
+        TogglePauseGAme();
+    }
+
     private void Awake()
     {
         Instance = this;
@@ -89,4 +103,18 @@ public class KitchenGameManager : MonoBehaviour
         return 1 -( gamePlayingTimer / gamePlayingTimerMax);
     }
 
+    public void TogglePauseGAme()
+    {
+        isGamePaused = !isGamePaused;
+        if (isGamePaused)
+        {
+            Time.timeScale = 0f;
+            OnGamePaused?.Invoke(this, EventArgs.Empty);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            OnGameUnPaused?.Invoke(this, EventArgs.Empty);
+        }
+    }
 }
